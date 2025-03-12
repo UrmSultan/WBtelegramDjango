47048f20f050c11c43027aca9a8940bb455dab3a
Urmanbetov Sultan <uurmanbetov@kstu.kg>
2025-03-12 17:28:00 +0600

    routes management + cleaning

---
+++ keyboards/__init__.py
@@ -0,0 +1,9 @@
+from .inline import product_keyboard, back_to_menu_button
+from .reply import auth_keyboard, main_menu_keyboard
+
+__all__ = [
+    "main_menu_keyboard",
+    "auth_keyboard",
+    "product_keyboard",
+    "back_to_menu_button"
+]
