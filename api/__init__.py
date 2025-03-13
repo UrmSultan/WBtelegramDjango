4b919ee739a865270a612dcaae371ba55c4aa5bb
Urmanbetov Sultan <uurmanbetov@kstu.kg>
2025-03-13 17:13:13 +0600

    Categories Added get_all_products function (no limit of 100 pcs). Added list of categories to “Show All” Added “Show All List” callback

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,12 @@
 from .supplier_info import get_supplier_name
-from .products import get_wb_products
+from .products import get_wb_products, get_all_products
 from .search_products import search_wb_products
 from .supplier_info import get_supplier_name
 
 __all__ = [
     "get_supplier_name",
     "get_wb_products",
+    "get_all_products",
     "search_wb_products",
     "get_supplier_name",
 ]
--- handlers/storage.py
+++ handlers/storage.py
@@ -1,2 +1,5 @@
 user_products = {}
 user_page = {}
+
+user_all_products = {}
+user_categories = {}
