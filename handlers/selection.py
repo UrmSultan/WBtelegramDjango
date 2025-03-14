e84be20eaec1a66c21f590417d1e0f26b2635e75
Urmanbetov Sultan <uurmanbetov@kstu.kg>
2025-03-14 16:25:09 +0600

    Sorted sizes list

--- handlers/selection.py
+++ handlers/selection.py
@@ -24,6 +24,27 @@ async def callback_product(call:CallbackQuery):
         await call.answer()
         return
 
+    SIZE_ORDER = {
+        "XXXS": 1, "XXS": 2, "XS": 3, "S": 4, "SM": 5, "M": 6, "L": 7, "XL": 8, "XXL": 9, "XXXL": 10, "4XL": 11,
+        "5XL": 12
+    }
+
+    def sort_key(size):
+        tech_size = size.get("techSize","NoSize")
+
+        # print(f"DEBUG: tech_size = {tech_size}, type = {type(tech_size)}")
+
+        if callable(tech_size):
+            tech_size = tech_size()
+
+        tech_size = str(tech_size).strip()
+        try:
+            return 0, float(str(tech_size))
+        except ValueError:
+            return 1,SIZE_ORDER.get(tech_size, 999), tech_size
+
+    sizes_sorted = sorted(sizes, key=sort_key)
+
     keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
             [
@@ -32,7 +53,7 @@ async def callback_product(call:CallbackQuery):
                     callback_data=f"size_{nm_id}_{size['chrtID']}_{size['skus'][0]}_{size['techSize'].replace(' ', '-')}"
                 )
             ]
-            for size in sizes if size.get("skus")
+            for size in sizes_sorted if size.get("skus")
         ]
     )
     await call.message.answer("Выберите размер:", reply_markup=keyboard)
